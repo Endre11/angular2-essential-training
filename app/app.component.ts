edaa8ed430fb357471b664b0fc17a162057b5cc9
Endre Csoregi <endre_csoregi@epam.com>
2018-12-03 09:08:01 +0100

    Add first component

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app',              // The selector property is what Angular will use to locate a custom HTML element, and apply the component too. (more later)
+                                //  -> there is an <app> cpmponent in index.html -> it will target that
+  template: '<h1>My App</h1>'   // Will fill with this the targeted custom element's inner HTML when processed
+})
+export class AppComponent {}
