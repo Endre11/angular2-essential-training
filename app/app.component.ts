a90281c04a497ac14068814ad53f6856bffbec56
Endre Csoregi <endre_csoregi@epam.com>
2018-12-03 11:20:30 +0100

    Add style

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,8 +5,11 @@ import { Component } from "@angular/core";
                                 // -> there is an <app> cpmponent in index.html -> it will target that
                                 //only loads on the first element it finds -> out of multiple <app> elements only one gets selected
                                 //mw (media watch) -> recommended to use a prefix (and at least one '-')
-  templateUrl: 'app/app.component.html' //Will fill with this the targeted custom element's inner HTML when processed
-                                        //' vs ` -> ` supports multiple lines
-                                        //templateUrl -> specify a file that contains the template
+  templateUrl: 'app/app.component.html',  //Will fill with this the targeted custom element's inner HTML when processed
+                                          //' vs ` -> ` supports multiple lines
+                                          //templateUrl -> specify a file that contains the template
+  styleUrls: ['app/app.component.css']    //add css
+                                          //takes an array of string
+                                          //stylesUrl also takes array
 })
 export class AppComponent {}
