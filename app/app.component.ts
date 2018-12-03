a9f0f5955ef19ef24a26fbe656dc2d5c9c366bd6
Endre Csoregi <endre_csoregi@epam.com>
2018-12-03 09:58:23 +0100

    Add templateUrl

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: 'mw-app',              //The selector property is what Angular will use to locate a custom HTML element, and apply the component too. (more later)
+  selector: 'mw-app',           //The selector property is what Angular will use to locate a custom HTML element, and apply the component too. (more later)
                                 // -> there is an <app> cpmponent in index.html -> it will target that
                                 //only loads on the first element it finds -> out of multiple <app> elements only one gets selected
                                 //mw (media watch) -> recommended to use a prefix (and at least one '-')
-  template: '<h1>My App</h1>'   //Will fill with this the targeted custom element's inner HTML when processed
+  templateUrl: 'app/app.component.html' //Will fill with this the targeted custom element's inner HTML when processed
+                                        //' vs ` -> ` supports multiple lines
+                                        //templateUrl -> specify a file that contains the template
 })
 export class AppComponent {}
